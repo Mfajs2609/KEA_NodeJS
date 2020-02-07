14ad1b85a3ba5bcbbedf8e8b9934ba990a96ce0b
Mfajs2609 <Mfajs@hotmail.dk>
2020-02-07 10:47:49 +0100

    Added the first exercise

---
+++ 2/1._Intro_Files/firstFile.js
@@ -0,0 +1,17 @@
+var name = "Jakub";
+
+console.log("Hello", name);
+
+var age = 123;
+
+console.log(name, "is", age, "years old");
+
+var anotherName = 'Al';
+
+//[]Square
+//{}Curly
+//:
+//;Semi kollon
+
+
+
